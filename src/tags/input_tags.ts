902934d5c31be93544e702d050fbd80b910219b4
Byron Murgatroyd <byron@omanom.com>
2024-09-03 09:53:32 +0100

    date inputs are now overwritten

--- src/tags/input_tags.ts
+++ src/tags/input_tags.ts
@@ -1,5 +1,5 @@
 import type { Tag, Branch } from "../types"
-import type { TagElement } from "../html"
+import type { TagElement, Element } from "../html"
 import { filterPass } from "../tag_utils"
 import * as utils from "../utils"
 import templateAttributes from "../attributes"
@@ -8,29 +8,34 @@ import { filterHTML } from "../filter"
 export const InputTag: Tag = {
 	name: "input",
 	render(el, cascade) {
-		// @NOTE Checkboxes have a slightly odd behaviour. HTML ticks a box if the
-		// checked tag exists but the dev needs a way to set it from a template.
-		// @TODO write test case for this.
-		if (utils.getAttribute(el, "type") === "checkbox") {
-			return async (ctx): Promise<Branch> => {
-				const checkedTmpl = utils.getAttribute(el, "checked")
-				const v = checkedTmpl ? ctx.getKey(checkedTmpl) : ""
-				const checked = !!v && v !== "off"
+		if (utils.getAttribute(el, "type") === "date") {
+			//
+			// If the type is a date then the value may have been
+			// templated as a full-date. In which case we just need
+			// to chop of the time.
+			//
 
-				const attrs = templateAttributes(el.attributes, ctx)
-				if (!checked) {
-					delete attrs["checked"]
-				} else {
-					attrs.checked = "on"
+			const childs = cascade.childs(el.elements)
+			return async (ctx) => {
+				const children = await childs(ctx)
+				const resp:Element = {
+					...el,
+					attributes: templateAttributes(el.attributes, ctx),
+					elements: children.elements,
 				}
 
-				return filterPass(ctx, el)
+				if (resp.attributes.value) {
+					const v = resp.attributes.value.toString()
+					resp.attributes.value = v.substr(0, 10)
+				}
+
+				return filterPass(children.ctx, resp)
 			}
+
 		} else {
-			// Otherwise it's just a normal html element
 			return filterHTML(el, cascade)
 		}
-	},
+	}
 }
 
 export const SelectTag: Tag = {
