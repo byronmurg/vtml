927b1e979ded67d1b5780c67c282bc015cf05f89
Byron Murgatroyd <byron@omanom.com>
2024-10-21 17:40:07 +0100

    Impreoved error output

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,7 @@ import VtmlDocument from "./document"
 import {serveVtmlDocument} from "./web"
 import type {exposeOptions} from "./web"
 import {printRenderDescription} from "./description"
+import printValidationErrors from "./validation_errors"
 import {program} from "commander"
 import DevMode from "./dev_mode"
 import starterTemplate from "./template"
@@ -13,6 +14,7 @@ program.name("vtml")
 	.argument("<file>", "html start file")
 	.option("--template", "Print the starter template")
 	.option("--dev", "Watch files for changes and restart")
+	.option("--validate", "Validate the document then exit")
 	.option("--describe", "Describe rendering")
 	.option("--listen <addr>", "Listen address")
 	.option("--port <port>", "Listen port")
@@ -31,9 +33,13 @@ program.name("vtml")
 		const initResult = VtmlDocument.LoadFromFile(file)
 
 		if (! initResult.ok) {
-			// @TODO Better than this
-			console.error(initResult.errors)
+			printValidationErrors(initResult.errors)
 			return process.exit(1)
+		}
+
+		if (options.validate) {
+			// As the results would already have been printed, just exit sucess.
+			return process.exit(0)
 		}
 
 		const vtmlDocument = initResult.result
---
+++ src/validation_errors.ts
@@ -0,0 +1,26 @@
+import { ValidationError } from "./types"
+
+type formatter = (msg:string) => string
+
+function colour(code:string): formatter {
+	return (msg:string) => `\x1b[${code}m${msg}\x1b[0m`
+}
+
+const red = colour("91")
+const yellow = colour("93")
+const cyan = colour("96")
+
+function printValidationError(error:ValidationError) {
+	console.error(
+		red("error"),
+		error.message,
+		`\n\t in <${error.tag}> at`,
+		`${cyan(error.filename)}:${yellow(error.linenumber.toString())}`,
+	)
+}
+
+export default
+function printValidationErrors(errors:ValidationError[]) {
+	errors.forEach(printValidationError)
+}
+
